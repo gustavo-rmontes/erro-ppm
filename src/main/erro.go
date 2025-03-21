d08f74efe2347f10ab668783a4e4320c2a680d24
gustavo <gustavoribeiromontes@gmail.com>
2025-03-20 22:44:36 -0300

    Wait for user to close the CLI

--- src/main/erro.go
+++ src/main/erro.go
@@ -7,13 +7,17 @@ const Million = 1000000
 func main() {
 	var mz_teorico, mz_observado, erro_ppm float32
 
-	fmt.Printf("Digite o m/z teórico: ")
-	fmt.Scan(&mz_teorico)
+	for {
+		fmt.Printf("Digite o m/z teórico: ")
+		fmt.Scan(&mz_teorico)
 
-	fmt.Printf("Digite o m/z observado: ")
-	fmt.Scan(&mz_observado)
+		fmt.Printf("Digite o m/z observado: ")
+		fmt.Scan(&mz_observado)
 
-	erro_ppm = (mz_observado - mz_teorico) / mz_teorico * Million
+		erro_ppm = (mz_observado - mz_teorico) / mz_teorico * Million
 
-	fmt.Printf("Erro (ppm) = %v\n", erro_ppm)
+		fmt.Printf("Erro (ppm) = %v\n", erro_ppm)
+
+		fmt.Println("Pressione Ctrl + C para sair.")
+	}
 }
