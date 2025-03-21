a2d53680f3ca7d49bffb07255fc407494ad7c3a3
gustavo <gustavoribeiromontes@gmail.com>
2025-03-20 22:26:01 -0300

    Erro ppm calculado

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gustavo-rmontes/erro_ppm
+
+go 1.24.1
---
+++ src/main/erro.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const Million = 1000000
+
+func main() {
+	var mz_teorico, mz_observado, erro_ppm float32
+
+	fmt.Printf("Digite o m/z teórico: ")
+	fmt.Scan(&mz_teorico)
+
+	fmt.Printf("Digite o m/z observado: ")
+	fmt.Scan(&mz_observado)
+
+	erro_ppm = (mz_observado - mz_teorico) / mz_teorico * Million
+
+	fmt.Printf("Erro (ppm) = %v\n", erro_ppm)
+}
